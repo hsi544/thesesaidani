f1264ef869e91f4701918de796da905f17e00018
tarik_saidani@yahoo.fr <tarik_saidani@yahoo.fr@ebba6fe4-ef18-11dd-9fdb-3debd5008f28>
2009-12-01 18:14:53 +0000

    performed rep cleanup

---
+++ thesesaidani/trunk/Manuscrit/Main/Chapter2/Code/pipecode.c
@@ -0,0 +1,6 @@
+template<class LS, class RS>
+expr<pipe, args<LS,RS> > 
+operator|( LS const&, RS const& )
+{
+  return expr<pipe, args<LS,RS> >();
+}
---
+++ thesesaidani/trunk/Manuscrit/Main/Chapter2/Code/ppekernel.c
@@ -0,0 +1,12 @@
+#include <skell.hpp>
+SKELL_KERNEL(sample,(2,(float const*,float*)));
+
+int main(int argc, char** argv)
+{
+ float in[256], out[256];
+
+ skell::environment(argc,argv);
+ sample(in,out);
+
+ return 0;
+}
